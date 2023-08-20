4727acca19ace02a05187d67136602d90bff36f2
mpbart <michael.barton0924@gmail.com>
2023-08-20 14:28:06 -0400

    Refactor core methods to use new DSL

--- impls/my_ruby_impl/core.rb
+++ impls/my_ruby_impl/core.rb
@@ -3,60 +3,76 @@ module Core
   class IndexOutOfRange < StandardError; end
 
   class << self
+    attr_accessor :ns
+
+    def add_builtin(symbol, &block)
+      ns[MalSymbolType.new(symbol)] = MalBuiltinType.new(symbol, &block)
+    end
+
     def ns
-      @ns ||= {
-        MalSymbolType.new('*') => MalBuiltinType.new('*') { |a, b| MalIntegerType.new(a.data * b.data)},
-        MalSymbolType.new('/') => MalBuiltinType.new('/') { |a, b| MalIntegerType.new(a.data / b.data)},
-        MalSymbolType.new('+') => MalBuiltinType.new('+') { |a, b| MalIntegerType.new(a.data + b.data)},
-        MalSymbolType.new('-') => MalBuiltinType.new('-') { |a, b| MalIntegerType.new(a.data - b.data)},
-        MalSymbolType.new('list') => MalBuiltinType.new('list') { |*args| MalListType.new(args) },
-        MalSymbolType.new('list?') => MalBuiltinType.new('list?') { |*args| MalBooleanFactory.to_boolean(args[0].is_a? MalListType) },
-        MalSymbolType.new('empty?') => MalBuiltinType.new('empty?') { |*args| MalBooleanFactory.to_boolean(args[0].data.empty?) },
-        MalSymbolType.new('count') => MalBuiltinType.new('count'){ |*args| MalIntegerType.new(args[0].is_a?(MalListType) ? args[0].data.count : args[0].count) },
-        MalSymbolType.new('=') => MalBuiltinType.new('='){ |*args| MalBooleanFactory.to_boolean(args[0].equals?(args[1])) },
-        MalSymbolType.new('<') => MalBuiltinType.new('<'){ |*args| MalBooleanFactory.to_boolean(args[0].data < args[1].data) },
-        MalSymbolType.new('<=') => MalBuiltinType.new('<='){ |*args| MalBooleanFactory.to_boolean(args[0].data <= args[1].data) },
-        MalSymbolType.new('>') => MalBuiltinType.new('>'){ |*args| MalBooleanFactory.to_boolean(args[0].data > args[1].data) },
-        MalSymbolType.new('>=') => MalBuiltinType.new('>='){ |*args| MalBooleanFactory.to_boolean(args[0].data >= args[1].data) },
-        MalSymbolType.new('pr-str') => MalBuiltinType.new('pr-str'){ |*args| MalStringType.new(args.map{ |s| Printer.pr_str(s, print_readably: true) }.join(' ')) },
-        MalSymbolType.new('str') => MalBuiltinType.new('str'){ |*args| MalStringType.new(args.map{ |s| Printer.pr_str(s, print_readably: false) }.join('')) },
-        MalSymbolType.new('prn') => MalBuiltinType.new('prn'){ |*args| puts args.map{ |arg| Printer.pr_str(arg, print_readably: true) }.join(' '); MalNilType.new(nil) },
-        MalSymbolType.new('println') => MalBuiltinType.new('println'){ |*args| puts args.map{ |arg| Printer.pr_str(arg, print_readably: false) }.join(' '); MalNilType.new(nil) },
-        MalSymbolType.new('read-string') => MalBuiltinType.new('read-string'){ |*args| Reader.read_str(args[0].data) },
-        MalSymbolType.new('slurp') => MalBuiltinType.new('slurp'){ |*args| MalStringType.new(File.open(args[0].data).read) },
-        MalSymbolType.new('atom') => MalBuiltinType.new('atom'){ |*args| MalAtomType.new(args[0]) },
-        MalSymbolType.new('atom?') => MalBuiltinType.new('atom?'){ |*args| MalBooleanFactory.to_boolean(args[0].is_a? MalAtomType) },
-        MalSymbolType.new('deref') => MalBuiltinType.new('deref'){ |*args| args[0].data },
-        MalSymbolType.new('@') => MalBuiltinType.new('@'){ |*args| args[0].data },
-        MalSymbolType.new('reset!') => MalBuiltinType.new('reset!'){ |*args| args[0].data = args[1]; args[1] },
-        MalSymbolType.new('swap!') => MalBuiltinType.new('swap!'){ |*args| args[0].data = args[1].call(args[0].data, *args[2..]); args[0].data },
-        MalSymbolType.new('cons') => MalBuiltinType.new('cons'){ |*args| MalListType.new([args[0]] + args[1].data) },
-        MalSymbolType.new('concat') => MalBuiltinType.new('concat'){ |*args| MalListType.new(args.each_with_object([]) { |i, acc| acc.concat(i.data) })},
-        MalSymbolType.new('nth') => MalBuiltinType.new('nth'){ |*args| raise Core::IndexOutOfRange unless args[0].data.length > args[1].data; args[0].data[args[1].data] },
-        MalSymbolType.new('first') => MalBuiltinType.new('first'){ |*args| Array(args[0].data).first || MalNilType.new(nil) },
-        MalSymbolType.new('rest') => MalBuiltinType.new('rest'){ |*args| MalListType.new(Array(Array(args[0].data)[1..])) },
-        MalSymbolType.new('throw') => MalBuiltinType.new('throw'){ |*args| raise MalExceptionType.new(args[0].data) },
-        MalSymbolType.new('apply') => MalBuiltinType.new('apply'){ |*args| args[0].call(*args[1...-1].map{ |i| i }.concat(args[-1].data)) },
-        MalSymbolType.new('map') => MalBuiltinType.new('map'){ |*args| MalListType.new(args[1].data.map{ |i| args[0].fn.call(i) }) },
-        MalSymbolType.new('nil?') => MalBuiltinType.new('nil?'){ |*args| MalBooleanFactory.to_boolean(args[0].is_a?(MalNilType)) },
-        MalSymbolType.new('true?') => MalBuiltinType.new('true?'){ |*args| MalBooleanFactory.to_boolean(args[0].is_a?(MalTrueType)) },
-        MalSymbolType.new('false?') => MalBuiltinType.new('false?'){ |*args| MalBooleanFactory.to_boolean(args[0].is_a?(MalFalseType)) },
-        MalSymbolType.new('symbol?') => MalBuiltinType.new('symbol?'){ |*args| MalBooleanFactory.to_boolean(args[0].is_a?(MalSymbolType)) },
-        MalSymbolType.new('keyword?') => MalBuiltinType.new('keyword?'){ |*args| MalBooleanFactory.to_boolean(args[0].is_a?(MalKeywordType)) },
-        MalSymbolType.new('symbol') => MalBuiltinType.new('symbol'){ |*args| MalSymbolType.new(args[0].data) },
-        MalSymbolType.new('keyword') => MalBuiltinType.new('keyword'){ |*args| args[0].is_a?(MalKeywordType) ? args[0] : MalKeywordType.new(':' + args[0].data) },
-        MalSymbolType.new('sequential?') => MalBuiltinType.new('sequential?'){ |*args| MalBooleanFactory.to_boolean(args[0].is_a?(MalSequentialType)) },
-        MalSymbolType.new('hash-map') => MalBuiltinType.new('hash-map'){ |*args| MalHashMapType.new(args) },
-        MalSymbolType.new('map?') => MalBuiltinType.new('map?'){ |*args| MalBooleanFactory.to_boolean(args[0].is_a?(MalHashMapType)) },
-        MalSymbolType.new('assoc') => MalBuiltinType.new('assoc'){ |*args| MalHashMapType.new(args[0].data + args[1..]) },
-        MalSymbolType.new('dissoc') => MalBuiltinType.new('dissoc'){ |*args| MalHashMapType.new(Array(args[0].remove_keys(args[1..]).map.reduce(&:concat))) },
-        MalSymbolType.new('get') => MalBuiltinType.new('get'){ |*args| args[0].get(args[1]) },
-        MalSymbolType.new('contains?') => MalBuiltinType.new('contains?'){ |*args| args[0].contains?(args[1]) },
-        MalSymbolType.new('keys') => MalBuiltinType.new('keys'){ |*args| MalListType.new(args[0].keys) },
-        MalSymbolType.new('vals') => MalBuiltinType.new('vals'){ |*args| MalListType.new(args[0].vals) },
-        MalSymbolType.new('vector?') => MalBuiltinType.new('vector?'){ |*args| MalBooleanFactory.to_boolean(args[0].is_a?(MalVectorType)) },
-        MalSymbolType.new('vector') => MalBuiltinType.new('vector'){ |*args| MalVectorType.new(args) },
-      }
+      @ns ||= {}
     end
   end
+
+  add_builtin('*')          { |*args| MalIntegerType.new(args[0].data * args[1].data)}
+  add_builtin('/')          { |*args| MalIntegerType.new(args[0].data / args[1].data)}
+  add_builtin('+')          { |*args| MalIntegerType.new(args[0].data + args[1].data)}
+  add_builtin('-')          { |*args| MalIntegerType.new(args[0].data - args[1].data)}
+  add_builtin('=')          { |*args| MalBooleanFactory.to_boolean(args[0].equals?(args[1])) }
+  add_builtin('<')          { |*args| MalBooleanFactory.to_boolean(args[0].data < args[1].data) }
+  add_builtin('>')          { |*args| MalBooleanFactory.to_boolean(args[0].data > args[1].data) }
+  add_builtin('@')          { |*args| args[0].data }
+  add_builtin('<=')         { |*args| MalBooleanFactory.to_boolean(args[0].data <= args[1].data) }
+  add_builtin('>=')         { |*args| MalBooleanFactory.to_boolean(args[0].data >= args[1].data) }
+  add_builtin('apply')      { |*args| args[0].call(*args[1...-1].map{ |i| i }.concat(args[-1].data)) }
+  add_builtin('assoc')      { |*args| MalHashMapType.new(args[0].data + args[1..]) }
+  add_builtin('atom')       { |*args| MalAtomType.new(args[0]) }
+  add_builtin('atom?')      { |*args| MalBooleanFactory.to_boolean(args[0].is_a? MalAtomType) }
+  add_builtin('conj')       { |*args| MalNilType.new(nil) }
+  add_builtin('concat')     { |*args| MalListType.new(args.each_with_object([]) { |i, acc| acc.concat(i.data) })}
+  add_builtin('cons')       { |*args| MalListType.new([args[0]] + args[1].data) }
+  add_builtin('contains?')  { |*args| args[0].contains?(args[1]) }
+  add_builtin('count')      { |*args| MalIntegerType.new(args[0].is_a?(MalListType) ? args[0].data.count : args[0].count) }
+  add_builtin('deref')      { |*args| args[0].data }
+  add_builtin('dissoc')     { |*args| MalHashMapType.new(Array(args[0].remove_keys(args[1..]).map.reduce(&:concat))) }
+  add_builtin('empty?')     { |*args| MalBooleanFactory.to_boolean(args[0].data.empty?) }
+  add_builtin('false?')     { |*args| MalBooleanFactory.to_boolean(args[0].is_a?(MalFalseType)) }
+  add_builtin('first')      { |*args| Array(args[0].data).first || MalNilType.new(nil) }
+  add_builtin('fn?')        { |*args| MalNilType.new(nil) }
+  add_builtin('get')        { |*args| args[0].get(args[1]) }
+  add_builtin('hash-map')   { |*args| MalHashMapType.new(args) }
+  add_builtin('keys')       { |*args| MalListType.new(args[0].keys) }
+  add_builtin('keyword')    { |*args| args[0].is_a?(MalKeywordType) ? args[0] : MalKeywordType.new(':' + args[0].data) }
+  add_builtin('keyword?')   { |*args| MalBooleanFactory.to_boolean(args[0].is_a?(MalKeywordType)) }
+  add_builtin('list')       { |*args| MalListType.new(args) }
+  add_builtin('list?')      { |*args| MalBooleanFactory.to_boolean(args[0].is_a? MalListType) }
+  add_builtin('map')        { |*args| MalListType.new(args[1].data.map{ |i| args[0].fn.call(i) }) }
+  add_builtin('map?')       { |*args| MalBooleanFactory.to_boolean(args[0].is_a?(MalHashMapType)) }
+  add_builtin('meta')       { |*args| MalNilType.new(nil) }
+  add_builtin('number?')    { |*args| MalNilType.new(nil) }
+  add_builtin('nil?')       { |*args| MalBooleanFactory.to_boolean(args[0].is_a?(MalNilType)) }
+  add_builtin('nth')        { |*args| raise Core::IndexOutOfRange unless args[0].data.length > args[1].data; args[0].data[args[1].data] }
+  add_builtin('pr-str')     { |*args| MalStringType.new(args.map{ |s| Printer.pr_str(s, print_readably: true) }.join(' ')) }
+  add_builtin('println')    { |*args| puts args.map{ |arg| Printer.pr_str(arg, print_readably: false) }.join(' '); MalNilType.new(nil) }
+  add_builtin('prn')        { |*args| puts args.map{ |arg| Printer.pr_str(arg, print_readably: true) }.join(' '); MalNilType.new(nil) }
+  add_builtin('str')        { |*args| MalStringType.new(args.map{ |s| Printer.pr_str(s, print_readably: false) }.join('')) }
+  add_builtin('readline')   { |*args|  puts Printer.pr_str(args[0]); MalStringType.new($stdin.readline.chomp) }
+  add_builtin('read-string'){ |*args| Reader.read_str(args[0].data) }
+  add_builtin('reset!')     { |*args| args[0].data = args[1]; args[1] }
+  add_builtin('rest')       { |*args| MalListType.new(Array(Array(args[0].data)[1..])) }
+  add_builtin('seq')        { |*args| MalNilType.new(nil) }
+  add_builtin('sequential?'){ |*args| MalBooleanFactory.to_boolean(args[0].is_a?(MalSequentialType)) }
+  add_builtin('slurp')      { |*args| MalStringType.new(File.open(args[0].data).read) }
+  add_builtin('string?')    { |*args| MalNilType.new(nil) }
+  add_builtin('swap!')      { |*args| args[0].data = args[1].call(args[0].data, *args[2..]); args[0].data }
+  add_builtin('symbol?')    { |*args| MalBooleanFactory.to_boolean(args[0].is_a?(MalSymbolType)) }
+  add_builtin('symbol')     { |*args| MalSymbolType.new(args[0].data) }
+  add_builtin('throw')      { |*args| raise MalExceptionType.new(args[0].data) }
+  add_builtin('time-ms')    { |*args| MalNilType.new(nil) }
+  add_builtin('true?')      { |*args| MalBooleanFactory.to_boolean(args[0].is_a?(MalTrueType)) }
+  add_builtin('vals')       { |*args| MalListType.new(args[0].vals) }
+  add_builtin('vector')     { |*args| MalVectorType.new(args) }
+  add_builtin('vector?')    { |*args| MalBooleanFactory.to_boolean(args[0].is_a?(MalVectorType)) }
+  add_builtin('with-meta')  { |*args| MalNilType.new(nil) }
+
 end
